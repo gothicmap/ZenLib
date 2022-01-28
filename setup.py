10b4d4f6c2aad907158dfcde631e3660f5348801
Eugene Chekanskiy <echekanskiy@gmail.com>
2022-01-28 17:05:39 +0200

    Some changes to setup script.

--- setup.py
+++ setup.py
@@ -120,9 +120,9 @@ class CMakeBuild(build_ext):
 # The information here can also be placed in setup.cfg - better separation of
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
-    name="ZenLib",
+    name="pyzenlib",
     version="0.0.1",
-    ext_modules=[CMakeExtension("cmake_example")],
+    ext_modules=[CMakeExtension("pyzenlib")],
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
     extras_require={"test": ["pytest>=6.0"]},
